b94233f5107e5b795927715451b0122964df5120
Dmitry Mozgin <m0391n@gmail.com>
2012-10-01 12:31:25 +0400

    First commit

---
+++ src/com/m039/magnifier/MagnifierActivity.java
@@ -0,0 +1,15 @@
+package com.m039.magnifier;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MagnifierActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
